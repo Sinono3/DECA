72d0afba1540c85c4021156cba44b1f6ed9f586c
Sinono3 <aldoacevedo@protonmail.com>
2026-04-10 15:21:33 +0800

    Change package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name="DECA",
+    name="decalib",
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
